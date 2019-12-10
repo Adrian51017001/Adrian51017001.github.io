379216611665df6e5a8972a0896b8409b09c52a8
Adrian51017001 <adrian_18@kharisma.ac.id>
2019-12-10 15:48:10 +0700

    fix sw.js

--- js/sw.js
+++ js/sw.js
@@ -9,13 +9,47 @@ var urlsToCache = [
       "/img/movie_1080.jpg",
       "/img/movie_720.jpg",
       "/img/movie_480.jpg",
-      "/css/style.css"
+      "/css/style.css",
 ];
 
 self.addEventListener("install", function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches
+    .match(event.request, { cacheName: CACHE_NAME })
+    .then(function(response) {
+      if (response) {
+        console.log("ServiceWorker: Gunakan aset dari cache: ", response.url);
+        return response;
+      }
+
+      console.log(
+        "serviceWorker: Memuat aset dari server: ",
+        event.request.url
+      );
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  eventwaitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            console.log("ServiceWorker: cache " + cacheName + " dihapus");
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
